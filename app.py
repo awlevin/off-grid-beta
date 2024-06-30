bae16974f2fdd1d3d04f2db6dce95664c611661a
Aaron Levin <awlevin@comcast.net>
2024-06-30 16:37:13 -0600

    handle twilio or postman requests

--- app.py
+++ app.py
@@ -8,7 +8,12 @@ app = Flask(__name__)
 
 @app.route('/sms', methods=['POST'])
 def sms_reply():
-  body = request.get_data('Body', as_text=True)
+  # Get body text from Twilio message if possible
+  body = request.values.get('Body', None)
+  if not body:
+    # Otherwise get body text as if normal post request
+    body = request.get_data('Body', as_text=True)
+
   resp = MessagingResponse()
 
   if body:
@@ -18,6 +23,7 @@ def sms_reply():
     
     if weather_data:
       formatted_forecast = format_weather_forecast(weather_data['list'])
+      print(formatted_forecast)
       resp.message(formatted_forecast)
     else:
       resp.message("Sorry, I couldn't find weather data for that location. Please try again with a different city name.")
